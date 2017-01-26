36f3e1e6b542f0c1f45fe49e8ccfcb06e1813af0
squash <josh@brokedown.net>
2017-01-26 15:20:46 +0000

    Add 5gbz channels

--- includes/functions.php
+++ includes/functions.php
@@ -119,18 +119,14 @@ function ParseConfig( $arrConfig ) {
 * @return $channel
 */
 function ConvertToChannel( $freq ) {
+  if ($freq<5000) {
   $channel = ($freq - 2407)/5;
-  if ($channel > 0 && $channel < 14) {
-    return $channel;
   } else {
-<<<<<<< Updated upstream
-=======
     $channel=($freq - 5000 )/5;
   }
   if ($channel > 0 && $channel < 196) {
       return $channel;
     } else {
->>>>>>> Stashed changes
     return 'Invalid Channel';
   }
 }
